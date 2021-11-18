c4dc00c6cf9a2500333f7b3dd8e96997556e8ad7
bojanabb <bojana.buljic@gmail.com>
2021-11-18 15:34:14 +0100

    Move store to src/ folder

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
-import configureStore from './redux/store'
-
-const store=configureStore();
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
