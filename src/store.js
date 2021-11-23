e94b993c17ab105d49550695ab6e821435f80b7b
bojanabb <bojana.buljic@gmail.com>
2021-11-23 16:31:22 +0100

    Update store, add thunkMiddleware

--- src/store.js
+++ src/store.js
@@ -1,14 +1,17 @@
-import { createStore} from "redux";
+import { createStore, applyMiddleware} from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from 'redux-thunk';
 import throttle from 'lodash.throttle';
 import rootReducer from "./redux/reducers";
 import { saveState, loadState} from './common/localeStorage'
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
 const persistedState=loadState();
 const store = createStore(
     rootReducer,
     persistedState,
-    composeWithDevTools()
+    composedEnhancer
 );
 
 store.subscribe(throttle(()=> {
